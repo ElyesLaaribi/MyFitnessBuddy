3295060c2f7220766387bd6aab21901251c956ab
ElyesLaaribi <elyeslaaribii@gmail.com>
2024-11-10 12:11:56 +0100

    added features

---
+++ app/src/main/java/com/example/myfitnessbuddy/SleepActivity.java
@@ -0,0 +1,70 @@
+package com.example.myfitnessbuddy;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class SleepActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sleep);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //get data
+        tabLayout = findViewById(R.id.tabLayout);
+        int selectedTabPosition = getIntent().getIntExtra("selected_tab_position", 0);
+        tabLayout.getTabAt(selectedTabPosition).select();
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int selectedTabPosition = tab.getPosition();
+                // Pass the selected tab position to the next activity
+                Intent intent = null;
+                switch (selectedTabPosition) {
+                    case 0:
+                        intent = new Intent(SleepActivity.this, MainActivity.class);
+                        break;
+                    case 1:
+                        intent = new Intent(SleepActivity.this, FoodActivity.class);
+                        break;
+                    case 2:
+                        intent = new Intent(SleepActivity.this, StepsActivity.class);
+                        break;
+                    case 3:
+                        intent = new Intent(SleepActivity.this, SleepActivity.class);
+                        break;
+                    case 4:
+                        intent = new Intent(SleepActivity.this, ExercisesActivity.class);
+                        break;
+                }
+                if (intent != null) {
+                    intent.putExtra("selected_tab_position", selectedTabPosition);
+                    startActivity(intent);
+                }
+            }
+            public void onTabUnselected(@NonNull TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(@NonNull TabLayout.Tab tab) {
+            }
+        });
+    }
+}
